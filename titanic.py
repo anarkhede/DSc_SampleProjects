e51b819617faaccba47b554c7219d58cffc47358
anarkhede <atulnarkhede2002@gmail.com>
2017-03-30 14:58:17 -0400

    Updated code 3/15/2017

--- titanic.py
+++ titanic.py
@@ -22,7 +22,14 @@ forest = RandomForestClassifier(n_estimators=3, random_state=0)
 forest.fit(X_train, y_train)
 print("Random Forest Train Score: ", forest.score(X_train, y_train))
 
-# 3) Read titanic_test.csv into a dataframe
+# 3) Test our model
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(forest, X_train, y_train, cv=10)
+print("Cross-validation scores: {}".format(scores))
+print("Average cross-va;idation scores: {}".format(scores.mean()))
+
+# 4) Read titanic_test.csv into a dataframe
 titanic_test = pd.read_csv('/Users/atul/Documents/INTERVIEW_PREP/Study/ML/ML_data/titanic_test.csv')
 titanic_test.drop(['Name', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
 titanic_test = titanic_test.dropna(axis=0)
@@ -31,7 +38,7 @@ titanic_test = titanic_test.replace(to_replace=['male', 'female'], value=[1, 0])
 
 X_test = titanic_test.ix[:, 1:titanic_test.shape[1]]
 
-# 4) Make Prediction
+# 5) Make Prediction
 predictions = forest.predict(X_test)
 pred_df = pd.DataFrame.from_records({'Predictions': predictions, 'PasID': pas_id})
 
